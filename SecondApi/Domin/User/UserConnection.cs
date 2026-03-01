eb5232a9eff6c12aaef19723cc7bf838c33721ab
Moqr134 <mohamadqasm68@gmail.com>
2026-03-01 03:54:14 +0300

    Add files via upload

---
+++ SecondApi/Domin/User/UserConnection.cs
@@ -0,0 +1,12 @@
+﻿namespace SecondApi.Domin.User
+{
+    public class UserConnection
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; } 
+        public required string ConnectionId { get; set; }
+        public string? DeviceToken { get; set; }
+        public string? role { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ SecondApi/Domin/User/UserConnectionMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SecondApi.Domin.User
+{
+    public class UserConnectionMap : IEntityTypeConfiguration<UserConnection>
+    {
+        public void Configure(EntityTypeBuilder<UserConnection> builder)
+        {
+            builder.ToTable("UserConnections","dbo");
+            builder.HasKey(uc => uc.Id);
+            builder.Property(uc=>uc.UserId);
+            builder.Property(uc=>uc.ConnectionId);
+            builder.Property(uc=>uc.DeviceToken);
+            builder.Property(uc => uc.IsActive)
+                   .HasDefaultValue(true)
+                   .IsRequired();
+            builder.Property(uc => uc.role);
+        }
+    }
+}
